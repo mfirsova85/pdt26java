38795b9e439f7fbc9998184ca7a6135627e0a9f0
marina firsova <firsova.m@gmail.com>
2015-12-16 23:33:04 +0300

    bat file for Lesson 5

---
+++ addressbook-selenium-tests/runtests.bat
@@ -0,0 +1,8 @@
+{\rtf1\ansi\ansicpg1251\cocoartf1344\cocoasubrtf720
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural
+
+\f0\fs22 \cf0 \CocoaLigature0 java -cp bin;/Users/marinka/Documents/pdt26java/addressbook-selenium-tests/Lib/xstream-1.4.8.jar;/Users/marinka/Documents/pdt26java/addressbook-selenium-tests/Lib/xstream/xpp3_min-1.1.4c.jar;/Users/marinka/Documents/pdt26java/addressbook-selenium-tests/Lib/xstream/xmlpull-1.1.3.1.jar;/Users/marinka/Downloads/selenium-server-standalone-2.48.2.jar org.testng.TestNG testng-customsuite.xml\
+}
